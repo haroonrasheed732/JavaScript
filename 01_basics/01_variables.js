ad3b230196129893b21446a277be5c10c3054b80
Haroon Rasheed <155428221+haroonrasheed732@users.noreply.github.com>
2026-05-18 07:13:49 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountPassword = "112233"
+accountCity = "karachi"
+
+// accountId = 2 #not allowed b/c const have fixed values
+accountEmail = "aa@gmail.com"
+accountPassword = "11"
+accountCity = "mhr"
+let accountstate 
+
+console.log(accountId);
+/*
+prefer not use to vr b/c of issue
+ in block scope and functional scope
+
+
+
+
+*/
+console.table([accountstate,accountEmail,accountCity,accountPassword])
